3b32d285f3749e823374514407a6ccdef4ddf10e
Julia Lindholm <juliamatilda.lindholm@gmail.com>
2019-10-16 14:16:02 +0200

    add search function
    add search/filtering function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
+
+
 $('.image-gallery').lightGallery({
   download:false,
   counter:false,
+  width:'90%'
 });
 
-// $('#aniimated-thumbnials').lightGallery({
-//     thumbnail:true
-// });
+
+
+
+
+
+
+
+
+//Searchfield//
+let $search = $('#filter');
+let $images = $('img');
+
+$search.on('keyup', function(){
+  let $value = $search.val().toLowerCase();
+  $images.show();
+  if($value !== ''){
+    $images.not('[alt*="' + $value + '"]').hide();
+  }
+
+});
